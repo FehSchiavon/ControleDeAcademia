41143d21701527388167ff199c5cff3ee8aa0ee8
FehSchiavon <felipschiavon@gmail.com>
2020-05-27 08:18:57 -0300

    core: Entendendo mais sobre Middlewares que e Encanamento a ligacao entre os arquivos

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/', function(req, res) {
-    return res.send('Bora codar...')
+    return res.render('about')
 })
 
 module.exports = routes
