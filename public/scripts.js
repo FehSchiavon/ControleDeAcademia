59f3867f0f0d8fcfa457fa35d0a3227fe035459d
FehSchiavon <felipschiavon@gmail.com>
2020-07-07 21:04:38 -0300

    Learn Includes

--- public/scripts.js
+++ public/scripts.js
@@ -1,5 +1,14 @@
 const currentPage = location.pathname
 const menuItems = document.querySelectorAll("header .links a")
 
+// for (item of menuItems) {
+//     if (currentPage == item.getAttribute("href")) {
+//         item.classList.add('active')
+//     }
+// }
+
+console.log("/instructors/2".includes(instructor))
+
+item.getAttribute("href").includes()
 
 console.log(currentPage)
