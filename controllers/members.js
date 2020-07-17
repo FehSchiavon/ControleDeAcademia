4ce04b0dd4a7aa375bdbb72331f2709cea8238b5
FehSchiavon <felipschiavon@gmail.com>
2020-07-17 20:21:35 -0300

    Create converter Blood

--- controllers/members.js
+++ controllers/members.js
@@ -6,24 +6,6 @@ const { age, date } = require('../utils')
 exports.index = function(req, res) {
     return res.render('members/index', { members: data.members })
 }
-// Show
-exports.show = function(req, res) {
-    // req.params
-    const { id } = req.params
-
-    const foundMember = data.members.find(function(member) {
-        return  id == member.id
-    })
-
-    if(!foundMember) return res.send('Member not found!')
-
-    const member = {
-        ...foundMember,
-        age: age(foundMember.birth),
-    }
-
-    return res.render('members/show', { member })
-}
 // Create
 exports.create = function(req, res) {
     return res.render('members/create')
@@ -63,6 +45,33 @@ exports.post = function(req, res) {
     })
 
     // return res.send(req.body)
+}
+// Show
+exports.show = function(req, res) {
+    // req.params
+    const { id } = req.params
+
+    const foundMember = data.members.find(function(member) {
+        return  id == member.id
+    })
+
+    const converterBlood = data.members.find(function(member) {
+        if ('A1' == member.blood) {
+            member.blood = 'A+'
+        } else if ('A' == member.blood) {
+
+        }
+    })
+
+    if(!foundMember) return res.send('Member not found!')
+
+    const member = {
+        ...foundMember,
+        age: age(foundMember.birth),
+    }
+    console.log(member)
+
+    return res.render('members/show', { member })
 }
 // Edit
 exports.edit = function(req, res) {
